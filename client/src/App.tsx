935c3403643641e872ff62340419130f703bad5e
Vishesh Dadhich <visheshdadhich20@gmail.com>
2025-03-04 23:46:43 +0530

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from "./components/mode-toggle";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen ">
+      <ModeToggle />
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
